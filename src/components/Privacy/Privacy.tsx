281feb12d357e3fe3801d5085e96a9c5297baf68
Yendric Van Roey <yendricvanroey@gmail.com>
2022-05-16 17:19:25 +0200

    Loading indicators + privacybeleid en voorwaarden

---
+++ src/components/Privacy/Privacy.tsx
@@ -0,0 +1,133 @@
+import { Container } from "@mui/material";
+import { FC } from "react";
+import Link from "@mui/material/Link";
+import { Link as RouterLink } from "react-router-dom";
+
+const Privacy: FC = () => {
+  return (
+    <Container>
+      <h1>Privacybeleid</h1>
+      <p>
+        Yendric Van Roey bouwde de Toodl app als een Open Source app. Deze DIENST wordt gratis ter beschikking gesteld
+        door Yendric Van Roey en is bedoeld voor gebruik zoals het is.
+      </p>
+      <p>
+        Deze pagina wordt gebruikt om bezoekers te informeren over mijn beleid met betrekking tot het verzamelen,
+        gebruiken en vrijgeven van Persoonlijke Informatie indien iemand beslist om mijn Service te gebruiken.
+      </p>
+      <p>
+        Indien u kiest om mijn Dienst te gebruiken, dan gaat u akkoord met het verzamelen en gebruik van informatie in
+        verband met dit beleid. De Persoonlijke Informatie die ik verzamel wordt gebruikt voor het verstrekken en
+        verbeteren van de Dienst. Ik zal uw informatie met niemand anders gebruiken of delen dan zoals beschreven in dit
+        Privacybeleid.
+      </p>
+      <p>
+        De termen die in dit Privacybeleid worden gebruikt, hebben dezelfde betekenis als in onze{" "}
+        <Link to="/algemene-voorwaarden" component={RouterLink}>
+          Algemene Voorwaarden
+        </Link>
+        , die toegankelijk zijn op Toodl, tenzij anders gedefinieerd in dit Privacybeleid.
+      </p>
+      <p>
+        <strong>Verzamelen en gebruik van informatie</strong>
+      </p>
+      <p>
+        Voor een betere ervaring, tijdens het gebruik van onze Service, kan ik je vragen om ons bepaalde persoonlijk
+        identificeerbare informatie te verstrekken, inclusief maar niet beperkt tot gebruikersnamen, e-mails,
+        wachtwoorden (hashed), todo&lsquo;s (en al hun eigenschappen: naam, datums, etc.), gebruikersinstellingen, todo
+        lijsten, smartschool agenda links. De informatie die ik vraag wordt bewaard op je apparaat en wordt op geen
+        enkele manier door mij verzameld.
+      </p>
+      <p>
+        <strong>Log Gegevens</strong>
+      </p>
+      <p>
+        Ik wil u informeren dat wanneer u gebruik maakt van mijn Dienst, in een geval van een fout in de app ik gegevens
+        en informatie verzamel (via producten van derden) op uw telefoon genaamd Log Data. Deze Logdata kunnen
+        informatie bevatten zoals het Internet Protocol (&quot;IP&quot;) adres van uw toestel, de naam van uw toestel,
+        de versie van uw besturingssysteem, de configuratie van de app bij het gebruik van mijn Service, de tijd en
+        datum van uw gebruik van de Service, en andere statistieken.
+      </p>
+      <p>
+        <strong>Cookies</strong>
+      </p>
+      <p>
+        Cookies zijn bestanden met een kleine hoeveelheid gegevens die gewoonlijk worden gebruikt als anonieme unieke
+        identificatoren. Deze worden naar uw browser gestuurd vanaf de websites die u bezoekt en worden opgeslagen in
+        het interne geheugen van uw apparaat.
+      </p>
+      <p>
+        Deze Dienst maakt niet expliciet gebruik van deze &quot;cookies&quot;. De app kan echter gebruik maken van code
+        en bibliotheken van derden die &quot;cookies&quot; gebruiken om informatie te verzamelen en hun diensten te
+        verbeteren. U hebt de mogelijkheid om deze cookies te aanvaarden of te weigeren en te weten wanneer een cookie
+        naar uw apparaat wordt gestuurd. Als u ervoor kiest om onze cookies te weigeren, kan het zijn dat u sommige
+        delen van deze Dienst niet kunt gebruiken.
+      </p>
+      <p>
+        <strong>Dienstverleners</strong>
+      </p>
+      <p>Ik kan om de volgende redenen gebruik maken van bedrijven en personen van derden:</p>
+      <ul>
+        <li>Om onze Service te vergemakkelijken;</li> <li>Om de Service namens ons te verlenen;</li>
+        <li>Om Service-gerelateerde diensten uit te voeren; of</li>
+        <li>Om ons te helpen bij het analyseren van hoe onze Service wordt gebruikt.</li>
+      </ul>
+      <p>
+        Ik wil gebruikers van deze Dienst informeren dat deze derden toegang hebben tot hun Persoonlijke Gegevens. De
+        reden hiervoor is om de taken uit te voeren die hen namens ons zijn toegewezen. Zij zijn echter verplicht de
+        informatie niet voor andere doeleinden openbaar te maken of te gebruiken.
+      </p>
+      <p>
+        <strong>Beveiliging</strong>
+      </p>
+      <p>
+        Ik waardeer uw vertrouwen in het verstrekken van uw Persoonlijke Informatie aan ons, daarom streven wij ernaar
+        om commercieel aanvaardbare middelen te gebruiken om deze te beschermen. Maar onthoud dat geen enkele methode
+        van transmissie over het internet, of methode van elektronische opslag 100% veilig en betrouwbaar is, en ik kan
+        de absolute veiligheid ervan niet garanderen.
+      </p>
+      <p>
+        <strong>Links naar andere sites</strong>
+      </p>
+      <p>
+        Deze Dienst kan links naar andere sites bevatten. Als u op een link van een derde partij klikt, wordt u naar die
+        site geleid. Merk op dat deze externe sites niet door mij worden beheerd. Daarom raad ik u ten zeerste aan het
+        Privacybeleid van deze websites te lezen. Ik heb geen controle over en neem geen verantwoordelijkheid voor de
+        inhoud, het privacybeleid, of de praktijken van sites of diensten van derden.
+      </p>
+      <p>
+        <strong>Privacy van kinderen</strong>
+      </p>
+      <div>
+        <p>
+          Ik verzamel niet bewust persoonlijk identificeerbare informatie van kinderen. Ik moedig alle kinderen aan om
+          nooit enige persoonlijk identificeerbare informatie in te dienen via de Applicatie en/of Diensten. Ik moedig
+          ouders en wettelijke voogden aan toezicht te houden op het internetgebruik van hun kinderen en te helpen bij
+          de handhaving van dit beleid door hun kinderen te instrueren nooit persoonlijk identificeerbare informatie te
+          verstrekken via de Applicatie en/of Diensten zonder hun toestemming. Indien u reden hebt om aan te nemen dat
+          een kind ons via de Applicatie en/of Diensten persoonlijk identificeerbare informatie heeft verstrekt, neem
+          dan contact met ons op. U moet ook ten minste 16 jaar oud zijn om toestemming te geven voor de verwerking van
+          uw persoonsgegevens in uw land (in sommige landen kunnen wij uw ouder of voogd toestaan dit namens u te doen).
+        </p>
+      </div>
+      <p>
+        <strong>Wijzigingen in dit privacybeleid</strong>
+      </p>
+      <p>
+        Ik kan ons Privacybeleid van tijd tot tijd bijwerken. U wordt daarom geadviseerd deze pagina regelmatig te
+        controleren op eventuele wijzigingen. Ik zal u op de hoogte brengen van eventuele wijzigingen door het nieuwe
+        Privacybeleid op deze pagina te plaatsen.
+      </p>
+      <p>Dit beleid is van kracht per 2022-05-14</p>
+      <p>
+        <strong>Contacteer Ons</strong>
+      </p>
+      <p>
+        Als u vragen of suggesties heeft over mijn Privacybeleid, aarzel dan niet om contact met mij op te nemen via
+        yendricvanroey@gmail.com.
+      </p>
+    </Container>
+  );
+};
+
+export default Privacy;
