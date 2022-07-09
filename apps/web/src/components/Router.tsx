05c56eb1e660e392fcade6476b66b3d528715d66
Yendric <yendricvanroey@gmail.com>
2022-07-09 16:10:26 +0200

    Lazy loading voor alle routes

--- apps/web/src/components/Router.tsx
+++ apps/web/src/components/Router.tsx
@@ -1,72 +1,64 @@
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import { FC, lazy, Suspense } from "react";
-import Todos from "./Todos/Todos";
-import Login from "./Login/Login";
-import Error404 from "./Errors/Error404";
-import Register from "./Register/Register";
-import Privacy from "./Privacy/Privacy";
-import AlgemeneVoorwaarden from "./AlgemeneVoorwaarden/AlgemeneVoorwaarden";
 import { CircularProgress, Stack } from "@mui/material";
 import ScrollToTop from "./Partials/ScrollToTop";
 
-const Planning = lazy(() => import("./Planning/Planning"));
-const Settings = lazy(() => import("./Settings/Settings"));
+const Planning = lazy(() => import("./Planning/PlanningContainer"));
+const Settings = lazy(() => import("./Settings/SettingsContainer"));
+const Error404 = lazy(() => import("./Errors/Error404"));
+const TodoContainer = lazy(() => import("./Todos/TodoContainer"));
+const LoginContainer = lazy(() => import("./Login/LoginContainer"));
+const RegisterContainer = lazy(() => import("./Register/RegisterContainer"));
+const PrivacyContainer = lazy(() => import("./Privacy/PrivacyContainer"));
+const AlgemeneVoorwaardenContainer = lazy(() => import("./AlgemeneVoorwaarden/AlgemeneVoorwaardenContainer"));
 
 const Router: FC = () => {
   return (
     <>
       <ScrollToTop />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/voorwaarden" element={<AlgemeneVoorwaarden />} />
+      <Suspense
+        fallback={
+          <Stack alignItems="center">
+            <CircularProgress size={65} sx={{ mt: 5 }}></CircularProgress>
+          </Stack>
+        }
+      >
+        <Routes>
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/register" element={<RegisterContainer />} />
+          <Route path="/privacy" element={<PrivacyContainer />} />
+          <Route path="/voorwaarden" element={<AlgemeneVoorwaardenContainer />} />
 
-        <Route
-          path="/settings"
-          element={
-            <PrivateRoute>
-              <Suspense
-                fallback={
-                  <Stack alignItems="center">
-                    <CircularProgress size={65} sx={{ mt: 5 }}></CircularProgress>
-                  </Stack>
-                }
-              >
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute>
                 <Settings />
-              </Suspense>
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/todos"
-          element={
-            <PrivateRoute>
-              <Todos />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/planning"
-          element={
-            <PrivateRoute>
-              <Suspense
-                fallback={
-                  <Stack alignItems="center">
-                    <CircularProgress size={65} sx={{ mt: 5 }}></CircularProgress>
-                  </Stack>
-                }
-              >
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/todos"
+            element={
+              <PrivateRoute>
+                <TodoContainer />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/planning"
+            element={
+              <PrivateRoute>
                 <Planning />
-              </Suspense>
-            </PrivateRoute>
-          }
-        />
+              </PrivateRoute>
+            }
+          />
 
-        <Route path="/" element={<Login />} />
-        <Route element={<Error404 />} />
-      </Routes>
+          <Route path="/" element={<LoginContainer />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
