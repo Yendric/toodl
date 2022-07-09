2be7ba9a3b8c8e77a32c44f787068646edb529ef
Yendric <yendricvanroey@gmail.com>
2022-07-09 16:10:17 +0200

    Correct bepalen van de laadtoestand + axios hook

---
+++ apps/web/src/hooks/useAxios.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useSnackbar } from "notistack";
+
+function useAxios() {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const apiUrl = process.env.REACT_APP_API_URL + "/v1" ?? "http://localhost/api/v1";
+
+  const api = axios.create({ baseURL: apiUrl });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const message = error.response.data.message ?? "Er is iets fout gegaan";
+      enqueueSnackbar(message, {
+        variant: "warning",
+      });
+      throw new Error(message);
+    }
+  );
+  return api;
+}
+
+export default useAxios;
