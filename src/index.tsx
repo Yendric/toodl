526ac367b3d3bafd4dd5993233c5888f475c6039
Yendric <yendricvanroey@gmail.com>
2022-10-01 15:59:46 +0200

    React scripts -> vite

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
 import { registerLicense } from "@syncfusion/ej2-base";
 
-registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE ?? "");
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE ?? "");
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -14,13 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
