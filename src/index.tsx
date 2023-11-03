9834a812a44f08a878d1e969f14f4b1d1ce2ad48
Yendric <yendricvanroey@gmail.com>
2023-11-04 00:08:14 +0100

    feat: auto reload pwa on update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   // <React.StrictMode>
