c7abc7aae4e1aedde557a1951d43e78ff5c6952d
Yendric <yendricvanroey@gmail.com>
2023-11-04 12:38:46 +0100

    fix: sta naast undef ook null toe non-req velden

--- src/schemas/todo.ts
+++ src/schemas/todo.ts
@@ -3,17 +3,17 @@ import { z } from "zod";
 export const storeSchema = z.object({
   done: z.boolean().default(false),
   subject: z.string().min(1).max(255),
-  description: z.string().max(255).default("").optional(),
-  enableDeadline: z.boolean().default(false).optional(),
-  isAllDay: z.boolean().default(false).optional(),
-  location: z.string().max(255).default("").optional(),
-  recurrenceRule: z.string().max(255).default("").optional(),
-  recurrenceException: z.string().max(255).default("").optional(),
-  startTimezone: z.string().max(255).default("").optional(),
-  endTimezone: z.string().max(255).default("").optional(),
+  description: z.string().max(255).default("").nullish(),
+  enableDeadline: z.boolean().default(false).nullish(),
+  isAllDay: z.boolean().default(false).nullish(),
+  location: z.string().max(255).default("").nullish(),
+  recurrenceRule: z.string().max(255).default("").nullish(),
+  recurrenceException: z.string().max(255).default("").nullish(),
+  startTimezone: z.string().max(255).default("").nullish(),
+  endTimezone: z.string().max(255).default("").nullish(),
   startTime: z.date().default(new Date()),
-  endTime: z.date().optional(),
-  listId: z.number().nullable().default(null).optional(),
+  endTime: z.date().nullish(),
+  listId: z.number().nullable().default(null).nullish(),
 });
 
 export const updateSchema = storeSchema.extend({
