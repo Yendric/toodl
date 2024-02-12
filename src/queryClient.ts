46015e7cd9a592ef7900ce847c58532a836eeb22
Yendric <yendricvanroey@gmail.com>
2024-02-12 13:43:36 +0100

    Refetch changes on focus change

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,6 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: "always",
+    },
+  },
   logger: {
     // Anders wordt de console volgespammed met 401 unauthorized errors, welke worden gebruikt om te weten of de gebruiker ingelogd is
     log: () => {},
