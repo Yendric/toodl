e4498463ec4b43bcc968c4d8060eec94ddd505c7
Yendric <yendricvanroey@gmail.com>
2025-09-17 00:28:24 +0200

    chore: enable strict mode in dev

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import React from "react";
 import { registerSW } from "virtual:pwa-register";
 registerSW({ immediate: true });
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
