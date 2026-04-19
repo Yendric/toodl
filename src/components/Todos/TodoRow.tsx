fd6c82f4e369daa4dcb4236c2c38f54eaeac742e
Yendric <yendricvanroey@gmail.com>
2026-04-19 12:37:25 +0200

    refactor: migrate drag-and-drop to @dnd-kit with iOS long-press and haptics

--- src/components/Todos/TodoRow.tsx
+++ src/components/Todos/TodoRow.tsx
@@ -1,4 +1,5 @@
-import { type DraggableProvided } from "@hello-pangea/dnd";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import { useState, type FC } from "react";
 import type { TodoResponse } from "../../api/generated/model";
 import TodoEditRow from "./TodoRowEdit";
@@ -6,19 +7,54 @@ import TodoShowRow from "./TodoRowShow";
 
 interface Props {
   todo: TodoResponse;
-  provided?: DraggableProvided;
-  isDragging?: boolean;
+  draggable?: boolean;
+  isOverlay?: boolean;
 }
 
-const TodoRow: FC<Props> = ({ todo, provided, isDragging }) => {
+const TodoRow: FC<Props> = ({ todo, draggable = false, isOverlay = false }) => {
   const [isEditing, setIsEditing] = useState(false);
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: todo.id,
+    disabled: !draggable,
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   const toggleEditing = () => setIsEditing(!isEditing);
 
   return isEditing ? (
-    <TodoEditRow todo={todo} toggleEditing={toggleEditing} provided={provided} isDragging={isDragging} />
+    <TodoEditRow
+      todo={todo}
+      toggleEditing={toggleEditing}
+      setNodeRef={setNodeRef}
+      style={style}
+      attributes={attributes}
+      listeners={listeners}
+      isDragging={isDragging}
+      isOverlay={isOverlay}
+    />
   ) : (
-    <TodoShowRow todo={todo} toggleEditing={toggleEditing} provided={provided} isDragging={isDragging} />
+    <TodoShowRow
+      todo={todo}
+      toggleEditing={toggleEditing}
+      setNodeRef={setNodeRef}
+      style={style}
+      attributes={attributes}
+      listeners={listeners}
+      isDragging={isDragging}
+      isOverlay={isOverlay}
+    />
   );
 };
 
